ba37797d3e6d055dd751cf0be1af8e8014b7a99f
mlucas2805 <mlucas.ml27@gmail.com>
2016-04-21 12:07:50 +0200

    preparation de l'admin

--- agency/includes/menu.php
+++ agency/includes/menu.php
@@ -28,38 +28,29 @@ include('entete.php');
                         <li><a href="armes.php">Armes & Maîtres d'armes</a></li>
                         <li><a href="regle&coti.php">Règlement & Cotisations</a></li>
                         <li><a href="album.php">Album photos</a></li>
-                        <li><a href="">Inscriptions & fichiers</a></li>
+                        <li><a href="">Inscriptions au club</a></li>
                     </ul>
                 </li>
                 <li><a href="contact.php">Contact</a></li>
                 <?php if(!isset($_SESSION['prenom'])){ // si aucune session n'est ouverte
                    include('connexion.php');}?>
             </ul>
-            <?php if(isset($_SESSION['prenom'])){ // si une session users est ouverte?>
-                <li><a href="presentation.php">Présentation de l'Escrime</a></li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Le Club<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="entrainement.php">Entrainements</a></li>
-                        <li><a href="armes.php">Les armes et les Maître d'armes</a></li>
-                        <li><a href="regle&coti.php">Règlement & Cotisations</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Manifestations<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="entrainement_spe.php">Entrainement spécial</a></li>
-                        <li><a href="tournois.php">Tournois Escrime</a></li>
-                        <li><a href="informations.php">Informations</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a data-toggle="dropdown" class="dropdown-toggle" href="#">Blog<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="news.php">News</a></li>
-                        <li><a href="matos_a_vendre.php">Matériels à vendre</a></li>
-                    </ul>
-                </li>
+            <?php if(isset($_SESSION['prenom'])){ // si une session admin est ouverte?>
+
+
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+                <!-- PARTIE ADMIN -->
+
+
+
+
                 <li><a href="contact.php">Contact</a></li>
                 <li id="logout"><a href="deconnexion.php">' .DECONNECT.'</a></li>
             <?php } ?>
